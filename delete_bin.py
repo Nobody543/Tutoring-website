5ce7b27121dab601b96a84a90f66e8db5e7da5f3
Nobody543 <92150906+Nobody543@users.noreply.github.com>
2026-04-17 12:33:12 +0100

    Update delete_bin.py

--- delete_bin.py
+++ delete_bin.py
@@ -1,8 +1,8 @@
 import requests
 url = 'https://api.jsonbin.io/v3/b/<69e1e75a856a682189431276>'
 headers = {
-  'X-Master-Key': '<69e1e5cb10716a4d5de5c45b>'
+  'X-Master-Key': '<ADD MASTER KEY>'
 }
-
+MASTER KEY: 69e1e5cb10716a4d5de5c45b
 req = requests.delete(url, json=None, headers=headers)
 print(req.text)
